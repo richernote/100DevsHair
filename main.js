151a92335e903c1918f82a889f3540ec11a32e59
richernote <richernote@gmail.com>
2022-05-05 22:26:14 -0700

    Further fixed image links and positon. Added extra media querries for tablets and mobile: 	changed nav to drop down 	adjusted position of CTA on main section 	adjusted the sizing for the video placeholder 	changed flex direction on artist section 	added close button for discount 	adjusted size of grid images at bottom of page fixed spelling error in HTML added JavaScript for dropdown menu

---
+++ main.js
@@ -0,0 +1,20 @@
+let menu = document.querySelector('.menu-button');
+let dropdown = document.querySelector('nav');
+
+let disClose = document.querySelector('.dis-close');
+let dis = document.querySelector('#discount-book');
+
+menu.addEventListener('click', peekABoo);
+disClose.addEventListener('click', close);
+
+function peekABoo() {
+    if (dropdown.style.display === "none") {
+        dropdown.style.display = "inline-block";
+    } else {
+        dropdown.style.display = "none";
+    }
+}
+
+function close() {
+    dis.style.display = "none";
+}
